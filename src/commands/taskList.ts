1ab11a55253c8985cb4c3ffde6f4d6c9e500491a
nickawrist <nicholas.aristizabal@gmail.com>
2025-02-05 16:09:39 -0500

    Task list command now prints out the user's current tasks and sorted in of decreasing priority

--- src/commands/taskList.ts
+++ src/commands/taskList.ts
@@ -2,21 +2,10 @@ import {
     SlashCommandBuilder,
     ChatInputCommandInteraction,
     EmbedBuilder,
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    MessageFlags, ButtonInteraction
+    MessageFlags
 } from 'discord.js';
-import { ObjectId } from 'mongodb';
 import type {Task, User} from "../types.ts";
-import {
-    createTask,
-    addTaskToUser,
-    getTaskById,
-    updateTask,
-    removeTaskFromUser,
-    completeTask, deleteTask
-} from "../database/taskRepository.ts";
+import {getTaskById} from "../database/taskRepository.ts";
 import {getUserById} from "../database/userRepository.ts";
 
 export const data = new SlashCommandBuilder()
@@ -25,8 +14,8 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction: ChatInputCommandInteraction) {
 
-    const user : User = await getUserById(interaction.user.id);
-    if(!user) {
+    const user : User | null = await getUserById(interaction.user.id);
+    if(!user || user.tasks.length === 0) {
         await interaction.reply({
             content: "You don't have any tasks yet. Create one with `/todo`",
             flags: MessageFlags.Ephemeral
@@ -36,33 +25,38 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 
     const tasks : Awaited<Task | null>[] = await Promise.all(user.tasks.map(async (taskId) => await getTaskById(taskId)));
 
+    // Sort tasks by priority value
+    const priorityLevels = { "low": 1, "medium": 2, "high": 3 };
+    tasks.sort((a, b) => {
+        if(!a || !b) return 0;
+        return priorityLevels[b.priority] - priorityLevels[a.priority];
+    });
+
     let taskList = "";
     tasks.forEach((task) => {
         if(!task) return;
         const priorityCapitalized = task.priority.charAt(0).toUpperCase() + task.priority.slice(1);
-
-        taskList += `**${task.title}** - ${task.description}\n**${priorityCapitalized}** - ${task.date}\n\n`;
+        let date = task.date || "No due date";
+        let taskListTemplate = taskList;
+        taskList += `**${task.title}** ${task.description}\n**Priority: ${priorityCapitalized}** \nDue: ${date}\n\n`;
+        if(taskList.length > 4000) {
+            taskList = taskListTemplate;
+            return;
+        }
     });
-
-    // Sort tasks by priority value
-    tasks.sort((a, b) => {
-        if(!a || !b) return 0;
-        const priorityValues = { low: 1, medium: 2, high: 3 };
-        return priorityValues[a.priority] - priorityValues[b.priority];
-    });
-
+    taskList += `\n----------\nTotal tasks: ${user.tasks.length}`;
 
 
     // Create an embed for the task
     const taskListsEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle('Your Tasks')
+        .setColor(0xFFA500)
+        .setTitle('Your Tasks\n----------')
         .setDescription(taskList);
-
 
     // Send embed with buttons
     await interaction.reply({
         embeds: [taskListsEmbed],
+        flags: MessageFlags.Ephemeral
     });
 
 }
